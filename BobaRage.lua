5822bd66aaeba398328e79e8dced8f839cf8fa37
Conistiree <30314751+Realheals@users.noreply.github.com>
2020-04-19 21:44:31 +1200

    Update BobaRage.lua

--- BobaRage.lua
+++ BobaRage.lua
@@ -1,9 +1,13 @@
-local frame = CreateFrame("Frame");
-frame:RegisterEvent("PLAYER_TARGET_CHANGED");
+local zgRage = CreateFrame("Frame");
+local mcRage = CreateFrame("Frame");
 
 NOISE_AMP = 10
 
-local function eventHandler(self, event, ...)
+zgRage:RegisterEvent("PLAYER_TARGET_CHANGED")
+mcRage:RegisterEvent("PLAYER_DEAD")
+
+
+local function zgRageEvent(self, event, ...)
 	if UnitName("target") == "Gurubashi Berserker" then
  		for i=0,NOISE_AMP,1 do 
  			PlaySoundFile("Interface/AddOns/BobaRage/BlueBerry.mp3", "Master")
@@ -11,5 +15,15 @@ local function eventHandler(self, event, ...)
  	end
 end
 
-frame:SetScript("OnEvent", eventHandler)
+local function mcRageEvent(self, event, ...)
+	instanceName = GetInstanceInfo()
+	if instanceName == "Molten Core" then
+		for i=0,NOISE_AMP,1 do 	
+			PlaySoundFile("Interface/AddOns/BobaRage/Reagents.mp3", "Master")
+		end
+	end 
+end
 
+
+zgRage:SetScript("OnEvent", zgRageEvent)
+mcRage:SetScript("OnEvent", mcRageEvent)
