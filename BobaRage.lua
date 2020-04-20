7b164f90e1a6209c594fd92b0e934b7c52f9cd19
Conistiree <30314751+Realheals@users.noreply.github.com>
2020-04-20 20:34:44 +1200

    Update BobaRage.lua

--- BobaRage.lua
+++ BobaRage.lua
@@ -1,13 +1,16 @@
 local zgRage = CreateFrame("Frame");
 local mcRage = CreateFrame("Frame");
+local loot = CreateFrame("Frame");
 
-NOISE_AMP = 10
+NOISE_AMP = 8
 
 zgRage:RegisterEvent("PLAYER_TARGET_CHANGED")
 mcRage:RegisterEvent("PLAYER_DEAD")
+loot:RegisterEvent("START_LOOT_ROLL")
 
 
 local function zgRageEvent(self, event, ...)
+	name = GetInstanceInfo()
 	if UnitName("target") == "Gurubashi Berserker" then
  		for i=0,NOISE_AMP,1 do 
  			PlaySoundFile("Interface/AddOns/BobaRage/BlueBerry.mp3", "Master")
@@ -24,6 +27,15 @@ local function mcRageEvent(self, event, ...)
 	end 
 end
 
+local function lootEvent(self, event, roll)
+	name = GetInstanceInfo()
+	_, info = GetLootRollItemInfo(roll)
+	if name == "Zul'Gurub" and (string.match(info, ".*Coin$") or string.match(info, ".*Bijou$")) then
+		RollOnLoot(roll, 2)
+	end
+end
+
 
 zgRage:SetScript("OnEvent", zgRageEvent)
 mcRage:SetScript("OnEvent", mcRageEvent)
+loot:SetScript("OnEvent", lootEvent)
